6d25c424632656691b684b8e130bc1a84164dc0d
raonieqr <raoniestevan@gmail.com>
2023-08-08 22:40:37 -0300

    add: CandidateTest

---
+++ src/test/entities/CandidateTest.java
@@ -0,0 +1,40 @@
+package test.entities;
+
+import entities.Candidate;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CandidateTest {
+	@Test
+	public void testAddCandidate() {
+		ArrayList<String> expectedSkills = new ArrayList<>();
+		expectedSkills.add("C");
+		Candidate candidate = new Candidate("Beto", "beto@gmail.com", expectedSkills, 18, "RJ",
+				"Desenvolvedor", "12344566701", 22785055);
+		ArrayList<Candidate> candidates = new ArrayList<>();
+		candidates.add(candidate);
+
+		assertTrue(candidates.size() == 1);
+	}
+
+	@Test
+	void testCandidateAttributes() {
+		ArrayList<String> expectedSkills = new ArrayList<>();
+		expectedSkills.add("C");
+		expectedSkills.add("C++");
+		expectedSkills.add("Java");
+		Candidate candidate = new Candidate("Beto", "beto@gmail.com", expectedSkills, 18, "RJ",
+				"Desenvolvedor", "12344566701", 22785055);
+		assertEquals("Beto", candidate.getName());
+		assertEquals("beto@gmail.com", candidate.getEmail());
+		assertArrayEquals(expectedSkills.toArray(), candidate.getSkills().toArray());
+		assertEquals(18, candidate.getAge());
+		assertEquals("RJ", candidate.getState());
+		assertEquals("Desenvolvedor", candidate.getDescription());
+		assertEquals("12344566701", candidate.getCpf());
+		assertEquals(22785055, candidate.getCep());
+	}
+}
