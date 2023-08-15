644a1eb76d104a7403cb931bcf4700981d21fb88
raonieqr <raoniestevan@gmail.com>
2023-08-14 23:56:29 -0300

    ts in progress

---
+++ frontend/candidate/candidate.js
@@ -0,0 +1,25 @@
+//intro_candidate.html
+var btnRegisterC = document.getElementById("register-c");
+var btnShowProfileC = document.getElementById("show-profile-c");
+btnRegisterC === null || btnRegisterC === void 0 ? void 0 : btnRegisterC.addEventListener("click", function () {
+    window.location.href = "./candidate_registration.html";
+});
+var modal = document.getElementById("modal");
+var behindModal = document.getElementById("behind-modal");
+btnShowProfileC === null || btnShowProfileC === void 0 ? void 0 : btnShowProfileC.addEventListener("click", function () {
+    if (modal)
+        modal.style.display = "flex";
+    if (behindModal)
+        behindModal.classList.add("modalBlur");
+});
+var btnExitModal = document.getElementById("exitModal");
+btnExitModal === null || btnExitModal === void 0 ? void 0 : btnExitModal.addEventListener("click", function () {
+    if (modal)
+        modal.style.display = "none";
+    if (behindModal)
+        behindModal.classList.remove("modalBlur");
+});
+var btnsigIn = document.getElementById("sigIn");
+btnsigIn === null || btnsigIn === void 0 ? void 0 : btnsigIn.addEventListener("click", function () {
+    window.location.href = "./candidate_profile.html";
+});
---
+++ frontend/candidate/candidate.ts
@@ -0,0 +1,32 @@
+//intro_candidate.html
+let btnRegisterC = document.getElementById("register-c");
+let btnShowProfileC = document.getElementById("show-profile-c");
+
+btnRegisterC?.addEventListener("click", function(): void {
+    window.location.href = "./candidate_registration.html";
+});
+
+
+let modal = document.getElementById("modal");
+let behindModal = document.getElementById("behind-modal");
+
+btnShowProfileC?.addEventListener("click", function(): void {
+    if (modal)
+    modal.style.display = "flex";
+if (behindModal)
+behindModal.classList.add("modalBlur");
+});
+
+let btnExitModal = document.getElementById("exitModal");
+
+btnExitModal?.addEventListener("click", function (): void {
+    if (modal)
+    modal.style.display = "none";
+if (behindModal)
+behindModal.classList.remove("modalBlur");
+});
+
+let btnsigIn = document.getElementById("sigIn");
+btnsigIn?.addEventListener("click", function(): void {
+    window.location.href = "./candidate_profile.html";
+});
