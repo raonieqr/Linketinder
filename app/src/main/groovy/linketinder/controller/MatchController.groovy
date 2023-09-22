e284314aa7eef645adc60eb31c92e633e9eaab1b
raonieqr <raoniestevan@gmail.com>
2023-09-22 10:45:16 -0300

    refactor: matchcontroller and matchview

--- app/src/main/groovy/linketinder/controller/MatchController.groovy
+++ app/src/main/groovy/linketinder/controller/MatchController.groovy
@@ -13,9 +13,10 @@ class MatchController {
 static void listAvailableVacancies(Candidate candidate, ArrayList<Vacancy> vacancies,
                                        MatchVacancyImpl matchVacancyImpl,
                                        int idMatch) {
+
     ArrayList<Integer> printedVacancyIds = new ArrayList<>()
-    boolean allVacanciesLiked = true
     Set<Integer> idsLiked =  new HashSet<>()
+    boolean allVacanciesLiked = true
 
         if (vacancies.isEmpty())
             VacancyView.displayNoVacancies()
@@ -25,28 +26,33 @@ static void listAvailableVacancies(Candidate candidate, ArrayList<Vacancy> vacan
                 boolean containsVacancie = false
 
                 candidate.getMatchVacancies().each { matchingVacancy ->
+
                     if (matchingVacancy.getVacancy().
                             getId() == vacancie.getId()) {
+
                         idsLiked.add(vacancie.getId())
+
                         containsVacancie = true
+
                         return
                     }
                 }
 
-                allVacanciesLiked = displayUnlikedVacancies(containsVacancie,
+                allVacanciesLiked = MatchView
+                        .displayUnlikedVacancies(containsVacancie,
                         printedVacancyIds, vacancie, allVacanciesLiked)
 
             }
         }
-
-        println(allVacanciesLiked)
 
         if (allVacanciesLiked)
             MatchView.displayAllVacanciesLiked()
         else {
                 MatchVacancy match = likeVacancy(candidate, vacancies,
                         idsLiked, idMatch)
+
                 if (match != null) {
+
                     matchVacancyImpl
                             .insertCandidateLiked(candidate,
                                     match.getVacancy().getId())
@@ -62,6 +68,7 @@ static void listAvailableVacancies(Candidate candidate, ArrayList<Vacancy> vacan
 
     static MatchVacancy likeVacancy(Candidate candidate, ArrayList<Vacancy> vacancies,
                             Set<Integer> idsLiked, int idMatch) {
+
         boolean containsNumber = true
 
         Vacancy vacancy
@@ -91,22 +98,4 @@ static void listAvailableVacancies(Candidate candidate, ArrayList<Vacancy> vacan
         return null
     }
 
-    static boolean displayUnlikedVacancies(boolean  containsVacancie,
-                                           ArrayList<Integer> printedVacancyIds,
-                                           Vacancy vacancy,
-                                           boolean allVacanciesLiked) {
-
-        if (!containsVacancie && !printedVacancyIds.contains(vacancy.getId())) {
-            allVacanciesLiked = false
-            printedVacancyIds.add(vacancy.getId())
-            println("Id da vaga: " + vacancy.getId())
-            println("Titulo: " + vacancy.getName())
-            println("Descrição: " + vacancy.getDescription())
-            println("Skills:")
-            println(vacancy.getSkills().join(", "))
-            println("------------------------------")
-        }
-
-        return allVacanciesLiked
-    }
 }
--- app/src/main/groovy/linketinder/view/MatchView.groovy
+++ app/src/main/groovy/linketinder/view/MatchView.groovy
@@ -33,9 +33,8 @@ class MatchView {
             }
         }
 
-        if (!isMatch) {
+        if (!isMatch)
             println("Ainda não houve match")
-        }
     }
 
     static void displayCompanyMatches(Company company) {
@@ -44,7 +43,9 @@ class MatchView {
             println("A sua empresa ainda não deu match")
         else {
             company.getMatchVacancies().each { match ->
+
                 if (match.getCompanyLiked()) {
+
                     print("Vaga: " + match.getVacancy().getName() + ", ")
 
                     match.getCandidate().showInfo()
@@ -56,7 +57,27 @@ class MatchView {
         }
     }
 
+    static boolean displayUnlikedVacancies(boolean  containsVacancie,
+                                           ArrayList<Integer> printedVacancyIds,
+                                           Vacancy vacancy,
+                                           boolean allVacanciesLiked) {
 
+        if (!containsVacancie && !printedVacancyIds.contains(vacancy.getId())) {
+
+            allVacanciesLiked = false
+
+            printedVacancyIds.add(vacancy.getId())
+            println("Id da vaga: " + vacancy.getId())
+            println("Titulo: " + vacancy.getName())
+            println("Descrição: " + vacancy.getDescription())
+            println("Skills:")
+            println(vacancy.getSkills().join(", "))
+
+            println("------------------------------")
+        }
+
+        return allVacanciesLiked
+    }
 
     static showLikedMsg(){
         println("Vaga curtida!")
